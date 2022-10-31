63d45c9186391efa4b4fdf63fd2ebb41ad51466f
xxf098 <xxf098@protonmail.com>
2022-10-30 21:54:11 -0400

    init project

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
 SHELL := bash# we want bash behaviour in all shell invocations
 
-dagger: # Build a dev dagger binary
+dag: # Build a dev dagger binary
 	CGO_ENABLED=0 go build -o ./cmd/dagflow -ldflags '-s -w' ./cmd/
--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,42 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/xxf098/dagflow/cmd/project"
+)
 
 func main() {
-	fmt.Println("main")
+	if len(os.Args) < 2 {
+		fmt.Println("dagflow version: v0.1.0")
+		fmt.Println(`useage:
+	dagflow init [project path]		
+`)
+		return
+	}
+	args := os.Args[1:]
+	if args[0] == "init" {
+		dir := "."
+		if len(args) > 1 {
+			dir = args[1]
+		}
+		projectInit(dir)
+		return
+	}
+}
+
+func projectInit(dir string) {
+
+	err := project.Init(context.Background(), dir, "")
+	if err != nil {
+		log.Fatal(err)
+	}
+	if dir == "." {
+		fmt.Println("Project initialized! To install dagger packages, run `dagflow update`")
+	} else {
+		fmt.Printf("Project initialized in \"%s\"! To install dagger packages, go to subfolder \"%s\" and run \"dagger project update\"", dir, dir)
+	}
 }
