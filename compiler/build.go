826f1042c473c871c95a74ca452b757638f05823
xxf098 <xxf098@protonmail.com>
2022-11-10 21:41:17 -0500

    add overlay

--- compiler/build.go
+++ compiler/build.go
@@ -3,6 +3,11 @@ package compiler
 import (
 	"context"
 	"errors"
+	"fmt"
+	"io/fs"
+	"os"
+	"path"
+	"path/filepath"
 
 	"cuelang.org/go/cue"
 	"cuelang.org/go/cue/cuecontext"
@@ -15,6 +20,33 @@ func Build(ctx context.Context, src string, args ...string) (*cue.Value, error) 
 	buildConfig := &cueload.Config{
 		Dir:     src,
 		Overlay: map[string]cueload.Source{},
+	}
+
+	fileSystem := os.DirFS(src)
+	err := fs.WalkDir(fileSystem, ".", func(p string, entry fs.DirEntry, err error) error {
+		if err != nil {
+			return err
+		}
+
+		if !entry.Type().IsRegular() {
+			return nil
+		}
+
+		if filepath.Ext(entry.Name()) != ".cue" {
+			return nil
+		}
+
+		contents, err := fs.ReadFile(fileSystem, p)
+		if err != nil {
+			return fmt.Errorf("%s: %w", p, err)
+		}
+
+		overlayPath := path.Join(buildConfig.Dir, p)
+		buildConfig.Overlay[overlayPath] = cueload.FromBytes(contents)
+		return nil
+	})
+	if err != nil {
+		return nil, err
 	}
 
 	instances := cueload.Instances(args, buildConfig)
