db3fef62606a296133c158615f195e5da831df12
xxf098 <xxf098@protonmail.com>
2022-11-14 20:58:17 -0500

    format error

--- plan/task/all.go
+++ plan/task/all.go
@@ -62,12 +62,12 @@ func (t *allTasks) Run(ctx context.Context, v *cue.Value) (*cue.Value, error) {
 			}()
 			t, err := Lookup(&v)
 			if err != nil {
-				lg.Error().Err(fmt.Errorf("Lookup error: %s", v.Path().String())).Msgf("index: %d name: %s", index, t.Name())
+				lg.Error().Err(fmt.Errorf("Lookup error: %s", v.Path().String())).Msgf("name: %s", t.Name())
 				return
 			}
 			_, err = t.Run(ctx, &v)
 			if err != nil {
-				lg.Error().Err(fmt.Errorf("Run error: %s", v.Path().String())).Msgf("index: %d name: %s", index, t.Name())
+				lg.Error().Err(fmt.Errorf("Run error: %s", v.Path().String())).Msgf("name: %s", t.Name())
 			}
 		}(ctx, i, task)
 	}
