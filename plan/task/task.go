75c08f9fbacd66084432355804a5adff395b363f
xxf098 <xxf098@protonmail.com>
2023-02-17 19:46:40 -0500

    run then

--- plan/task/task.go
+++ plan/task/task.go
@@ -159,10 +159,7 @@ func Then(ctx context.Context, v *cue.Value) error {
 	tk := tv.Kind()
 	if tk.IsAnyOf(cue.StructKind) && tv.IsConcrete() {
 		return runThenTask(ctx, tv, isBackground)
-	}
-
-	// list tasks
-	if tv.IncompleteKind().IsAnyOf(cue.ListKind) {
+	} else if tv.IncompleteKind().IsAnyOf(cue.ListKind) {
 		iter, err := tv.List()
 		if err != nil {
 			return err
